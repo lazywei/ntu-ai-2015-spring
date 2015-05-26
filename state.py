163f11228e3a34ab886f2dbbdc9520ce1ea268e1
kuohsuanlo <kuohsuanlo@gmail.com>
2015-05-26 17:34:56 +0800

    getSucc done

--- state.py
+++ state.py
@@ -33,27 +33,63 @@ class State:
     cars = []
     mapMaxX = 0
     mapMaxY = 0
-    ###Need a function to read from map and decide, now hard coded.
+    
+    ######################
+    #  In map 
+    #  0 = road
+    #  -1= obstacle
+    #  >=1 car
+    ######################
 
     def __init__(self):
         print '__init__(self)'
-
+    
+    
     def loadMap(self):
         self.currentMap = np.loadtxt('map.out')
         self.mapMaxX = len(self.currentMap)
         self.mapMaxY = len(self.currentMap[0])
 
+    
+    #Done
     def getMap(self):
         return self.currentMap
-
+    #Done
     def getCars(self):
         return self.cars
+    
+    #Not yet QQ
+    def getSucc(self,carId):
+        actions = []        
+        ##north
+        nextX = self.cars[carId].location[0]-1
+        nextY = self.cars[carId].location[1]
+        if nextX >=0 :
+            if self.currentMap[nextX][nextY] ==0 : 
+                actions.append('north')
+        ##south
+        nextX = self.cars[carId].location[0]+1
+        nextY = self.cars[carId].location[1]
+        if nextX <self.mapMaxX :
+            if self.currentMap[nextX][nextY] ==0 : 
+                actions.append('south')
+        ##west
+        nextX = self.cars[carId].location[0]
+        nextY = self.cars[carId].location[1]-1
+        if nextY >=0 :
+            if self.currentMap[nextX][nextY] ==0 : 
+                actions.append('west')
+        ##east
+        nextX = self.cars[carId].location[0]
+        nextY = self.cars[carId].location[1]+1
+        if nextY <self.mapMaxY :
+            if self.currentMap[nextX][nextY] ==0 : 
+                actions.append('east')
+                
+                      
+        return actions
 
-    def getSucc(carId):
-        print 'getSucc(carId)'
-        #return possible actions
-
-    def getStateByAction(carId, action):
+    def getStateByAction(self,carId, action):
         print 'getStateByAction(carId, action)'
         #move carId according to the action
         # dynamics happen here
@@ -64,15 +100,15 @@ class State:
    #################################################################
     def generateCars(self,n_cars):
         
-        for i in range( n_cars):
+        for i in range( n_cars+1):
             sx = random.choice(np.arange(self.mapMaxX)) 
             sy = random.choice(np.arange(self.mapMaxY)) 
             
             dx = random.choice(np.arange(self.mapMaxX)) 
             dy = random.choice(np.arange(self.mapMaxY))
-            self.cars.append(  car(  i,'vehicle',(sx,sy)  ,  (sx,sy)   ,  (dx,dy)  , 0 , 1)  )
+            self.cars.append(  car(  i+1,'vehicle',(sx,sy)  ,  (sx,sy)   ,  (dx,dy)  , 0 , 1)  )
 
-            self.currentMap[sx][sy] = i
+            self.currentMap[sx][sy] = i+1
     def printMap(self,map):
         for row in map:
             for c in row:
@@ -91,7 +127,9 @@ if __name__ == '__main__':
     a = State()
     a.loadMap()
     map = a.getMap()
-    a.generateCars(100)
+    a.generateCars(50)
     a.printMap(map)
+    for i in range(50):
+        print a.getSucc(i)
     #for c in a.getCars():
     #    c.toString()
